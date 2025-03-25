404b13af1734ff49bf13b1f82257a48c01f29db2
pr-juan-k <djjuankalexis18@gmail.com>
2025-03-25 17:15:40 -0300

    trabaje con punteros

--- tp1-1.c
+++ tp1-1.c
@@ -2,5 +2,13 @@
 
 int main(){
     printf("Hola mundo");
+    int num = 1;
+    int *punt;
+    punt = num;
+    printf("\nEl contenido del puntero:%d",*punt);
+    printf("\nLa direccion de memoria que guarda el puntero:%p",punt);
+    printf("\nLa direccion de memoria de la variable:%d",&num);
+    printf("\nLa direccion de memoria del puntero:%d",&punt);
+    printf("\nTamanio de memoria de la variable:%d",sizeof(num));
     return 0;
 }
